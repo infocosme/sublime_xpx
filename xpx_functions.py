89b680dcef7b17aba5f7fc4b28b2763588a41546
pmilon <31013006+pmilon@users.noreply.github.com>
2022-04-15 17:06:07 +0200

    Add files via upload

--- xpx_functions.py
+++ xpx_functions.py
@@ -5,95 +5,127 @@ import sublime
 import sublime_plugin
 
 
-def addGotoFunctionInContextMenuXPX():
+def getListFunctionName(myview):
+	maliste = []
+	# Parcours de toutes les occurences de texte comprenant <function name=.
+	for myregionfunction in myview.find_all("<function name=\""):
+		# A compter de la fin de la région trouvée, extension de la région avec séparateur ".
+		myname = myview.substr(myview.expand_by_class(myregionfunction.end()+1,sublime.CLASS_WORD_START | sublime.CLASS_WORD_END,"\""))
+		# Ajout de la ligne de commande dans le sous-menu XPX du menu contextuel.
+		maliste.append(myname)
+	return maliste
+
+
+class XpxContextMenuFindFunctionDefCommand(sublime_plugin.TextCommand):
 	"""
-	Le sous-menu est organisé en deux parties optionnelles :
-		- Partie 1 : Clic-droit sur un function exec : création raccourci vers function name correspondant
-		- Partie 2 : Liste des function name défini dans la page courante.
-
-	Recherche toutes les balises <function name=""> du script en cours.
-	Ajoute au menu contextuel toutes les entrées.
-
-	Lors du clic sur un nom de fonction, déclenche l'affichage de la ligne correspondante.
-
-	A partir de la window courante, lecture de la view active.
-	Recherche de toutes les occurences contenant <function name=.
-	Parcours des occurences, affichage du contenu de chaque ligne.
-
-	Le sous-menu créé dynamiquement est ajouté au cache, pas dans le dossier d'installation du package.
-	Le sous-menu complète donc le sous-menu défini dans le package.
-
-	Les commandes du context_menu seront ajoutées au menu id xpx en tant que children.
-
-	Pour atteindre tous les contextes, suppression des arguments d'entrées et fondation sur sublime.active_window.
-	"""
-	encoding = 'utf-8'
-	#print("coucou")
-
-	# make sure cache path exists
-	cache_path = os.path.join(sublime.cache_path(), "XPX")
-	os.makedirs(cache_path, exist_ok=True)
-
-	# Récupération de la vue active (celle affichée).
-	mywindow = sublime.active_window()
-	myview = mywindow.active_view()
-
-	if not myview.match_selector(myview.window_to_text((0,0)), "text.html.xpx"):
-		#print("hors XPX")
-		# Pour effacer le menu éventuel déjà créé : écriture d'une occurence vide.
-		context_menu = []
-		# write the context menu item to the cache path : w et non pas w+.
-		with open(os.path.join(cache_path, "Context.sublime-menu"), "w") as cache:
-			cache.write(json.dumps(context_menu, cache))
+	Commande volatile dans le menu contextuel.
+	Proposition d'atteindre la définition de la fonction sur laquelle le clic-droit est effectué (n'importe où sur la ligne).
+	Recherche du contenu de toute la balise cliquée puis contrôle si la balise est "function" puis "exec" puis lancement commande.
+	"""	
+	def getFunctionName(self, event):
+		# Récupération du nom de la fonction concernée.
+		pt = self.view.window_to_text((event["x"], event["y"]))
+		# Est-ce que nous sommes sur une balise function ?
+		if self.view.match_selector(pt,'meta.tag.block.any.xpx'):
+			# Lecture du début de la balise function.
+			ptfindfunction = self.view.expand_by_class(pt,sublime.CLASS_PUNCTUATION_START | sublime.CLASS_PUNCTUATION_END,"<>").begin()+1
+			if (self.view.substr(self.view.expand_by_class(ptfindfunction,sublime.CLASS_WORD_START | sublime.CLASS_WORD_END)) == "function"):
+				ptfindexec = self.view.expand_by_class(ptfindfunction,sublime.CLASS_WORD_START | sublime.CLASS_WORD_END).end()+3
+				if (self.view.substr(self.view.expand_by_class(ptfindexec,sublime.CLASS_WORD_START | sublime.CLASS_WORD_END)) == "exec"):
+					ptfindname = self.view.expand_by_class(ptfindexec,sublime.CLASS_WORD_START | sublime.CLASS_WORD_END).end()+3
+					myname = self.view.substr(self.view.expand_by_class(ptfindname,sublime.CLASS_WORD_START+sublime.CLASS_WORD_END,"\""))
+					return myname
+		# Dans les autres cas, on annule l'option.
 		return None
 
-	else:
-		#print("script XPX")
-		context_menu = [
-	    	{ "caption": "-" },
-			{ 
-	        	"caption": "XPX",
-				"children": []
-			}
-		]
-		# Parcours de toutes les occurences de texte comprenant <function name=.
-		affmenu = False
-		for myregionfunction in myview.find_all("<function name=\""):
-			# A compter de la fin de la région trouvée, extension de la région avec séparateur ".
-			myname = myview.substr(myview.expand_by_class(myregionfunction.end(),sublime.CLASS_WORD_START,"\""))
-			# Ajout de la ligne sous-menu dans le json texte.
-			context_menu[1]['children'].append({ "caption": "Go to function name "+myname[6:len(myname)-2], "command": "xpx_go_to_function_name", "args": {"functionName": myname} })
-			# print(myname[6:len(myname)-2])
-			affmenu = True
-		if not affmenu:
-			context_menu = []
-		# print(json.dumps(context_menu, sort_keys=True))
-		# write the context menu item to the cache path : w et non pas w+.
-		with open(os.path.join(cache_path, "Context.sublime-menu"), "w") as cache:
-			cache.write(json.dumps(context_menu, cache))
+	def want_event(self):
+		return True
 
+	def is_visible(self, event):
+		return self.getFunctionName(event) is not None
+	
+	def description(self, event):
+		functionName = self.getFunctionName(event)
+		# Limitation de la longueur du texte dans le menu
+		if len(functionName) > 64:
+			functionName = functionName[0:64] + "..."
+		return "Goto Definition " + functionName
 
-class XpxGoToFunctionNameCommand(sublime_plugin.WindowCommand):
-	"""
-	Recherche de la chaîne name="[nom de la fonction]" puis déplacement de l'écran.
-	"""
-
-	# Dans le cas d'une commande, il est possible de transférer des arguments par args en jSON.
-	# Le champ défini dans args est récupérable en ajoutant le même champ au prototype de la fonction.
-	def run(self, functionName):
-		
-		#print("Go to function name "+functionName)
-		# Recherche de l'emplacement visé par le functionName : name="nomFonction".
-		myview = self.window.active_view()
-		# Déplacement de l'écran sur l'emplacement trouvé.
-		myview.show_at_center(myview.find(functionName,0))
+	def run(self, edit, event):
+		flags = sublime.ENCODED_POSITION | sublime.FORCE_GROUP
+		# Récupération du nom de la fonction ciblée.
+		functionName = self.getFunctionName(event)
+		mywindow = sublime.active_window()
+		# Recherche d'une entrée symbol correspondant au nom de la fonction.
+		mylocations=mywindow.symbol_locations(functionName,sublime.SYMBOL_SOURCE_INDEX,sublime.SYMBOL_TYPE_DEFINITION)
+		if (len(mylocations)>0):
+			# En cas de file trouvé, affichage de chacun des files trouvés (normalement un seul).
+			for l in mylocations:
+				mywindow.open_file(l.path_encoded_position(), flags)
 
 
 class EventListener(sublime_plugin.EventListener):
-
+	"""
+	Ajout d'une écoute d'event pour déclencher la gestion du menu contextuel à chaque clic-droit.
+	Valable uniquement pour les function name présents dans le view en cours.
+	ATTENTION : le menu contextuel ne peut être modifié qu'ici.
+	A l'intérieur d'un TextCommand, le menu contextuel est déjà ouvert, donc si modifications il y a,
+	ces modifications n'apparaitraient qu'au prochain affichage : au prochain clic-droit.
+	"""
 	def on_text_command(self, view, command, args):
 		# ST wants to display the context menu
 		if command == "context_menu":
-			# Dans ce cas, mise à jour du menu contextuel XPX...
-			addGotoFunctionInContextMenuXPX()
+			"""
+			Commande volatile dans le menu contextuel.
+			Le sous-menu ne se déclenche qu'en cas de fichier possédant une région syntaxe XPX.
+			Recherche toutes les balises <function name=""> du script en cours.
+			Ajoute au menu contextuel toutes les entrées correspondantes.
+			La commande correspondante à chaque entrée déplace l'affichage sur la ligne correspondante (show_center).
+			Fonctionnement :
+				Recherche de toutes les occurences contenant <function name=.
+				Parcours des occurences, insertion du contenu de chaque ligne au menu contextuel.
+				Si aucune occurence trouvée, création d'un sous-menu artificiel vide (pour effacement).
+				Le sous-menu créé dynamiquement est ajouté au cache, pas dans le dossier d'installation du package.
+				Le sous-menu complète donc le sous-menu défini dans le package.
+			A chaque déclenchement, le sous menu contextuel est entièrement ré-écrit ce qui provoque son effacement le cas échéant.
+			"""
+			if view.match_selector(view.window_to_text((0,0)), "text.html.xpx"):
+				# Recherche d'éventuelles occurences.
+				maliste = getListFunctionName(view)
+				if (len(maliste)>0):
+					# Initialisation du menu contextuel à afficher.
+					context_menu = [
+						{ 
+				        	"caption": "Show Function",
+							"children": []
+						}
+					]
+					# Parcours de toutes les occurences de texte comprenant <function name=.
+					for mafonction in maliste:
+						# Ajout de la ligne de commande dans le sous-menu XPX du menu contextuel.
+						context_menu[0]['children'].append({ "caption": mafonction, "command": "xpx_goto_function_name", "args": {"functionName": mafonction} })
+				else:
+					context_menu = []
+			else:
+				context_menu = []
+			# make sure cache path exists
+			cache_path = os.path.join(sublime.cache_path(), "XPX")
+			os.makedirs(cache_path, exist_ok=True)
+			# write the context menu item to the cache path : w et non pas w+.
+			# Ecriture systématique du context-menu pour actualisation en permanence (à chaque changement de view).
+			encoding = 'utf-8'
+			with open(os.path.join(cache_path, "Context.sublime-menu"), "w") as cache:
+				cache.write(json.dumps(context_menu, cache))
 
+
+class XpxGotoFunctionNameCommand(sublime_plugin.WindowCommand):
+	"""
+	Déclenchement du scroll de la view courante sur la définition du function name demandé.
+	Dans le cas d'une commande, il est possible de transférer des arguments par args en jSON.
+	Le champ défini dans args est récupérable en ajoutant le même champ au prototype de la fonction.
+	"""
+	def run(self, functionName):
+		# Recherche de l'emplacement visé par le functionName : name="nomFonction".
+		myview = self.window.active_view()
+		# Déplacement de l'écran sur l'emplacement trouvé.
+		myview.show_at_center(myview.find("name=\""+functionName+"\"",0))
